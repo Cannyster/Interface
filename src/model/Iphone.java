d534a5f839e65b8c840165177d74e2d3a752cd38
Jhonanthan Campos <dionathancampos@hotmail.com>
2024-05-01 17:51:38 -0300

    first commit

---
+++ src/model/Iphone.java
@@ -0,0 +1,63 @@
+package model;
+
+import interfaces.AparelhoTelefonico;
+import interfaces.Navegador;
+import interfaces.ReprodutorMusical;
+
+public class Iphone implements AparelhoTelefonico, ReprodutorMusical, Navegador {
+
+	@Override
+	public void exibirPagina() {
+		
+		
+	}
+
+	@Override
+	public void adicionarNovaAba() {
+		
+		
+	}
+
+	@Override
+	public void atualizarPagina() {
+		
+		
+	}
+
+	@Override
+	public void tocar() {
+		
+		
+	}
+
+	@Override
+	public void pausar() {
+		
+		
+	}
+
+	@Override
+	public void selecionarMusica() {
+		
+		
+	}
+
+	@Override
+	public void ligar() {
+		
+		
+	}
+
+	@Override
+	public void atender() {
+		
+		
+	}
+
+	@Override
+	public void iniciarCorreioVoz() {
+		
+		
+	}
+
+}
